1d1e4b91b32b4a2a38459512aa5fc417f672c88e
Shyam Agrawal <Shyam.Agrawal@L-P-A.com>
2022-10-29 13:24:53 +0200

    Add embed youtube videos to dashboard

--- js/food.js
+++ js/food.js
@@ -1,7 +1,9 @@
 const foods = [
+       "Palak Panner",
+       "Besan Chilla",
+       "Toast Sandwich",
        "Moong Dal (Yellow)",
        "Massor Dal",
-       "Palak Panner",
        "Chawli Ki Sabji",
        "Allo Ki Sabji",
        "Bangan",
@@ -12,11 +14,9 @@ const foods = [
        "Simla Mirchi Besan",
        "Poha",
        "Rawa",
-       "Besan Chilla",
        "Rawa Chilla",
        "Maggie",
        "Bhel",
-       "Toast Sandwich",
        "Vegetable Sandwich",
        "Channa Dal",
        "Cabbbage Ki Sabji",
@@ -28,3 +28,90 @@ const foods = [
        "Matar Panner",
        "Mix Veg With Panner"
     ]
+
+var foods_json = [
+                     { "food_name" : "Palak Panner",
+                       "food_youtube_link" : "https://www.youtube.com/embed/b5bDtzWCR9M" 
+                     },
+                     { "food_name" : "Besan Chilla",
+                       "food_youtube_link" : "https://www.youtube.com/embed/4lvADI1s0L0" 
+                     },
+                     { "food_name" : "Toast Sandwich",
+                       "food_youtube_link" : "https://www.youtube.com/embed/FTrojLwEA8I" 
+                     },
+                     { "food_name" : "Moong Dal Yellow",
+                       "food_youtube_link" : "https://www.youtube.com/embed/K58aXvkJmYg" 
+                     },
+                     { "food_name" : "Masoor Dal Black",
+                       "food_youtube_link" : "https://www.youtube.com/embed/sa3Rq4g39Cw" 
+                     },
+                     { "food_name" : "Chawli Ki Sabji",
+                       "food_youtube_link" : "https://www.youtube.com/embed/fZpFv0CkXoA" 
+                     },
+                     { "food_name" : "Allo Ki Sabji",
+                       "food_youtube_link" : "https://www.youtube.com/embed/RSeQGkvjAxY" 
+                     },
+                     { "food_name" : "Aloo Baingan",
+                       "food_youtube_link" : "https://www.youtube.com/embed/vq8xQnlYL6E" 
+                     },
+                     { "food_name" : "French Beans",
+                       "food_youtube_link" : "https://www.youtube.com/embed/qwaT8L3pzBk" 
+                     },
+                     { "food_name" : "Laal Masoor - Orange Dal (High Protein)",
+                       "food_youtube_link" : "https://www.youtube.com/embed/qZzrSma8160" 
+                     },
+                     { "food_name" : "Moong Chawal",
+                       "food_youtube_link" : "https://www.youtube.com/embed/HVbWsffuUTk" 
+                     },
+                     { "food_name" : "Kadhi Chawal",
+                       "food_youtube_link" : "https://www.youtube.com/embed/YehXayddgck" 
+                     },
+                     { "food_name" : "Simla Mirchi Besan",
+                       "food_youtube_link" : "https://www.youtube.com/embed/4WoKfR0DhsM" 
+                     },
+                     { "food_name" : "Poha",
+                       "food_youtube_link" : "https://www.youtube.com/embed/VKvwWeVy8Nw" 
+                     },
+                     { "food_name" : "Rawa Upma",
+                       "food_youtube_link" : "https://www.youtube.com/embed/W4mnN7Fcaz0" 
+                     },
+                     { "food_name" : "Rawa Chilla",
+                       "food_youtube_link" : "https://www.youtube.com/embed/CUbG0o8OiUw" 
+                     },
+                     { "food_name" : "Maggie",
+                       "food_youtube_link" : "https://www.youtube.com/embed/F141lLt4usI" 
+                     },
+                     { "food_name" : "Bhel",
+                       "food_youtube_link" : "https://www.youtube.com/embed/Fm5jqBXIJq4" 
+                     },
+                     { "food_name" : "Vegetable Sandwich",
+                       "food_youtube_link" : "https://www.youtube.com/embed/B1shAs4I5X8" 
+                     },
+                     { "food_name" : "Channa Dal",
+                       "food_youtube_link" : "https://www.youtube.com/embed/QD9me4tKCUk" 
+                     },
+                     { "food_name" : "Cabbbage Ki Sabji",
+                       "food_youtube_link" : "https://www.youtube.com/embed/Bwa4ctUvo4w" 
+                     },
+                     { "food_name" : "Roti and Paneer ki Sabzi",
+                       "food_youtube_link" : "https://www.youtube.com/embed/7Kzym2aTCw8" 
+                     },
+                     { "food_name" : "Chole",
+                       "food_youtube_link" : "https://www.youtube.com/embed/uKUvtyjw8R0" 
+                     },
+                     { "food_name" : "Rajma",
+                       "food_youtube_link" : "https://www.youtube.com/embed/HYuf33JSEHk" 
+                     },
+                     { "food_name" : "Only Onion Ki Sabji",
+                       "food_youtube_link" : "https://www.youtube.com/embed/xI6krFU-ZF8" 
+                     },
+                     { "food_name" : "Pasta",
+                       "food_youtube_link" : "https://www.youtube.com/embed/rBmSraF9am0" 
+                     },
+                     { "food_name" : "Matar Panner",
+                       "food_youtube_link" : "https://www.youtube.com/embed/m-BiLpoyo_o" 
+                     },
+                     { "food_name" : "Mix Veg With Panner",
+                       "food_youtube_link" : "https://www.youtube.com/embed/dDQy0xAwWl8" 
+                     }
+                  ];
--- js/randomizer.js
+++ js/randomizer.js
@@ -1,11 +1,26 @@
+var randomNumber;
+var foods_from_json;
+
+var foods_json_stringify = JSON.stringify(foods_json);
+foods_from_json = JSON.parse(foods_json_stringify);
 function food_randomizer() {
-  var randomNumber = getRandomInt(0, foods.length);
-  console.log(foods[randomNumber]);
-  document.getElementById("food_display").innerHTML = foods[randomNumber];
+  randomNumber = getRandomInt(0, foods_from_json.length);
+  console.log(randomNumber);
+  console.log(foods_from_json);
+  document.getElementById("food_display").innerHTML = foods_from_json[randomNumber]['food_name'];
+  Checkout_Recipe();
 }
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+
+function Checkout_Recipe() {  
+  var youtube_div = document.getElementById("youtube_div");
+  youtube_div.style.display = "block";
+
+  var youtube_iframe = document.getElementById("youtube_iframe");
+  youtube_iframe.setAttribute("src", foods_from_json[randomNumber]['food_youtube_link']);
 }
